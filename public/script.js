c88baeafdc799a2861f170f9905791007d737a3c
Anson Chew <ansonchewhl@gmail.com>
2023-05-10 11:09:03 +1000

    added a function to notice the users when they have not enough movie data & added a function to check if the users have reached the bottom of the webpage to highlight they have reached the last section & better formated the script.js into sections

--- public/script.js
+++ public/script.js
@@ -1,143 +1,70 @@
-// Functions to call when the page finishes loading
-document.addEventListener('DOMContentLoaded', function () {
-    displayMovie();
-    sectionOffsetCheck();
-});
-
-// Nav-bar animation
-// Create a variable to reference the toggle <button>
-var navbarToggle = navbar.querySelector("#navbar-toggle");
-
-// Create a variable to reference the nav menu container <div>
-var navbarMenu = document.querySelector("#navbar-menu");
-
-// Create a variable to reference the <ul> list of nav links
-var navbarLinksContainer = navbarMenu.querySelector(".navbar-links");
-
-// Add or remove the 'active' class on the toggle <button> when clicked
-navbarToggle.addEventListener("click", () => { navbarToggle.classList.toggle('active') });
-
-// Remove the 'active' class on the menu container <div> when clicked 
-// This will close the menu if the user clicks outside the nav link <ul>
-navbarMenu.addEventListener("click", () => { navbarToggle.classList.remove('active') });
-
-// Stop clicks on the navbar links from toggling the menu (for when it's not mobile)
-navbarLinksContainer.addEventListener("click", (e) => e.stopPropagation());
-
-// Function for the nav bar current selection underliner
 const allNavLinks = document.querySelectorAll(".navbar-link");
 const allSections = document.querySelectorAll(".main-section"); // Selecting all the key section headers
 let sectionOffset = [0, 0, 0, 0];
 let currentSection = 0;
 
-// A function that recorded all sections' offset position
-function sectionOffsetCheck() {
-    for (var i = 0; i < 4; i++) {
-        sectionOffset[i] = allSections[i].offsetTop;
-        // console.log(i + ": " + allSections[i].offsetTop);
-    }
-}
+const form = document.getElementById('movie-form');
+
+let showAll = false;
+const showAllBtn = document.getElementById('show-all-btn');
+
+const mainGenre = ["action", "adventure", "comedy", "fantasy", "horror", "romance", "sci-fi", "war"];
+
+
+
+
+
+// Functions to call when the page finishes loading
+document.addEventListener('DOMContentLoaded', function () {
+    navbarAnimation();
+    contentUpdate();
+});
 
 window.onscroll = function (event) {
     // console.log(window.pageYOffset);
-    for (var i = 0; i < 4; i++) {
+
+    const totalSections = sectionOffset.length;
+    for (var i = 0; i < totalSections; i++) {
         // Check whether the scroll position arrived a new section
         if (window.pageYOffset >= sectionOffset[i] && i != currentSection) {
+            // Function for the nav bar current selection underliner
             // Change the id attribute to restyle the nav bar and current selection
             allNavLinks[currentSection].removeAttribute("id");
             allNavLinks[i].setAttribute("id", "current-section");
             currentSection = i;
         }
     }
+
+    // Check if the users have reached the bottom part inspired by https://stackoverflow.com/questions/3898130/check-if-a-user-has-scrolled-to-the-bottom-not-just-the-window-but-any-element
+    // The concept was made to deal with the situation which the last section is less than 100vh
+    var lastSectionHeight = document.body.scrollHeight - sectionOffset[totalSections - 1];
+    var lastSectionPoint = document.body.scrollHeight - window.innerHeight; // The whole document height minus the viewport height
+    if(window.pageYOffset + lastSectionHeight == lastSectionHeight + lastSectionPoint) {
+        allNavLinks[currentSection].removeAttribute("id");
+        allNavLinks[totalSections - 1].setAttribute("id", "current-section");
+        currentSection = totalSections - 1;
+    }
 };
 
-// A function that will be triggered when the users submit the new movie form
-// Submit function has to be added to the form instead of the button: https://stackoverflow.com/questions/32637920/why-does-a-submit-event-listener-not-work-on-a-submit-button
-const form = document.getElementById('movie-form');
-form.addEventListener("submit", function (event) {
-    event.preventDefault();
-
-    let movieLst = JSON.parse(localStorage.getItem('movieLst')) || [];
-    movieLst.push(getMovieDetails());
-    movieLst.sort(watchedDateOrder);
-    localStorage.setItem('movieLst', JSON.stringify(movieLst));
-    console.log(JSON.parse(localStorage.getItem('movieLst')));
-    form.reset();
-
+function contentUpdate() {
     displayMovie();
-    movieData();
-
-    let url = window.location.href;
-    if (url.indexOf("#") != -1) { url = url.split('#')[0] }
-    window.location.href = url + "#history";
-});
-
-function getMovieDetails() {
-    const movieName = document.querySelector('input[name="movieName"]').value;
-    const movieGenre = document.querySelector('input[name="movieGenre"]').value;
-    const watchedDate = document.querySelector('input[name="watchedDate"]').value;
-    const movieDuration = document.querySelector('input[name="movieDuration"]').value;
-    const movieRating = document.querySelector('input[name="rating"]:checked').value;
-    const movieComment = document.querySelector('input[name="movieComment"]').value;
-    const uid = generateUUID();
-
-    // Get date function https://www.freecodecamp.org/news/javascript-get-current-date-todays-date-in-js/
-    const date = new Date();
-    let d = date.getDate();
-    let m = date.getMonth() + 1;
-    let y = date.getFullYear();
-    const commentDate = `${d}/${m}/${y}`;
-
-    const movie = {
-        movieName: movieName, movieGenre: movieGenre, watchedDate: watchedDate,
-        movieDuration: movieDuration, movieRating: movieRating, movieComment: movieComment,
-        commentDate: commentDate, uid: uid
-    }
-
-    return movie
+    plotData();
+    favData();
+    sectionOffsetCheck(); // Re-calculate the offset position of each section
 }
 
-// A function that helps to sort the objects by their watched date https://stackoverflow.com/questions/1129216/sort-array-of-objects-by-string-property-value
-function watchedDateOrder(a, b) {
-    if (a.watchedDate < b.watchedDate) {
-        return 1;
-    }
-    if (a.watchedDate > b.watchedDate) {
-        return -1;
-    }
-    return 0;
-}
 
-// Use to generate a unique set of id for the objects, which makes the tracking process easier
-function generateUUID() {
-    let uuid = '';
-    const randomValues = new Uint8Array(16);
-    crypto.getRandomValues(randomValues);
-    randomValues[6] &= 0x0f;
-    randomValues[6] |= 0x40;
-    randomValues[8] &= 0x3f;
-    randomValues[8] |= 0x80;
-    uuid += randomValues.slice(0, 4).join('');
-    uuid += '-';
-    uuid += randomValues.slice(4, 6).join('');
-    uuid += '-';
-    uuid += randomValues.slice(6, 8).join('');
-    uuid += '-';
-    uuid += randomValues.slice(8, 10).join('');
-    uuid += '-';
-    uuid += randomValues.slice(10).join('');
-    return uuid;
-}
 
+// Main functions
 function displayMovie() {
     const historyLst = document.getElementById('history-lst');
     let movieLst = JSON.parse(localStorage.getItem('movieLst')) || [];
 
-    while (historyLst.firstChild) historyLst.removeChild(historyLst.firstChild); // Removing all childs
-
+    removeChilds(historyLst);
+    
     if (movieLst.length < 1) {
         let notice = document.createElement("h3");
-        notice.setAttribute("id", "notice");
+        notice.setAttribute("class", "notice");
         notice.innerText = "You have not yet added any movie...";
         historyLst.appendChild(notice);
     } else {
@@ -207,76 +134,24 @@ function displayMovie() {
             parentDiv.appendChild(seperateLine);
         });
     }
-
-    // Re-calculate the offset position of each section
-    sectionOffsetCheck();
-    movieData();
 }
 
-const mainGenre = ["action", "adventure", "comedy", "fantasy", "horror", "romance", "sci-fi", "war"];
-function setGenreIcon(img, genre) {
-    img.setAttribute("class", "movie-img");
+function plotData() {
+    const profileData = document.getElementById('profile-data');
+    const profileDataNone = document.getElementById('profile-data-none');
 
-    if (mainGenre.includes(genre)) {
-        img.setAttribute("src", `image/section_history/icons/${genre.toLowerCase()}.png`);
-        img.setAttribute("alt", `An icon that represents the movie is ${genre.toLowerCase()} genre`);
-    } else {
-        img.setAttribute("src", `image/section_history/icons/other.png`);
-        img.setAttribute("alt", "An icon with a unpacked box and question mark in the middle");
-    }
-
-    return img
-}
-
-let showAll = false;
-const showAllBtn = document.getElementById('show-all-btn');
-showAllBtn.addEventListener("click", () => {
-    const history = document.getElementById('history');
-
-    if (!showAll) {
-        showAllBtn.innerText = "Show Less"
-        history.style.maxHeight = "none";
-        history.style.overflow = "visible";
-        showAll = true;
-    } else {
-        showAllBtn.innerText = "Show All"
-        history.style.maxHeight = "100vh";
-        history.style.overflow = "hidden";
-        showAll = false;
-    }
-
-    sectionOffsetCheck();
-})
-
-function clearHistory() {
-    // Confirmation panel for destructive action https://www.codexworld.com/how-to/show-delete-confirmation-message-dialog-javascript/
-    var confirmation = confirm("Are you sure to clear ALL the history?");
-
-    if (confirmation) {
-        localStorage.removeItem('movieLst');
-        displayMovie();
-    }
-}
-
-function removeMovie(obj) {
     let movieLst = JSON.parse(localStorage.getItem('movieLst')) || [];
-    let id = obj.parentNode.parentNode.id;
 
-    for (var i = 0; i < movieLst.length; i++) {
-        if (movieLst[i].uid == id) {
-            movieLst.splice(i, 1);
-            console.log("Movie has been removed");
-            break;
-        }
+    if (movieLst.length < 1) {
+        profileData.style.display = "none";
+        profileDataNone.style.display = "block";
+    } else {
+        profileData.style.display = "flex";
+        profileDataNone.style.display = "none";
     }
-
-    localStorage.setItem('movieLst', JSON.stringify(movieLst));
-    console.log(JSON.parse(localStorage.getItem('movieLst')));
-
-    displayMovie();
 }
 
-function movieData() {
+function favData() {
     const movieLst = JSON.parse(localStorage.getItem('movieLst')) || [];
 
     var allGenre = [];
@@ -307,10 +182,87 @@ function movieData() {
         }
     });
 
-    data.sort(sortFavGenre);
+    data.sort(favGenreOrder);
     // console.log(data);
 
     favRecommendation(data);
+}
+
+
+
+
+
+// Sub functions
+// A function that recorded all sections' offset position
+function sectionOffsetCheck() {
+    for (var i = 0; i < 4; i++) {
+        sectionOffset[i] = allSections[i].offsetTop;
+        // console.log(i + ": " + allSections[i].offsetTop);
+    }
+}
+
+function getMovieDetails() {
+    const movieName = document.querySelector('input[name="movieName"]').value;
+    const movieGenre = document.querySelector('input[name="movieGenre"]').value;
+    const watchedDate = document.querySelector('input[name="watchedDate"]').value;
+    const movieDuration = document.querySelector('input[name="movieDuration"]').value;
+    const movieRating = document.querySelector('input[name="rating"]:checked').value;
+    const movieComment = document.querySelector('input[name="movieComment"]').value;
+    const uid = generateUUID();
+
+    // Get date function https://www.freecodecamp.org/news/javascript-get-current-date-todays-date-in-js/
+    const date = new Date();
+    let d = date.getDate();
+    let m = date.getMonth() + 1;
+    let y = date.getFullYear();
+    const commentDate = `${d}/${m}/${y}`;
+
+    const movie = {
+        movieName: movieName, movieGenre: movieGenre, watchedDate: watchedDate,
+        movieDuration: movieDuration, movieRating: movieRating, movieComment: movieComment,
+        commentDate: commentDate, uid: uid
+    }
+
+    return movie
+}
+
+// Use to generate a unique set of id for the objects, which makes the tracking process easier
+function generateUUID() {
+    let uuid = '';
+    const randomValues = new Uint8Array(16);
+    crypto.getRandomValues(randomValues);
+    randomValues[6] &= 0x0f;
+    randomValues[6] |= 0x40;
+    randomValues[8] &= 0x3f;
+    randomValues[8] |= 0x80;
+    uuid += randomValues.slice(0, 4).join('');
+    uuid += '-';
+    uuid += randomValues.slice(4, 6).join('');
+    uuid += '-';
+    uuid += randomValues.slice(6, 8).join('');
+    uuid += '-';
+    uuid += randomValues.slice(8, 10).join('');
+    uuid += '-';
+    uuid += randomValues.slice(10).join('');
+    return uuid;
+}
+
+function setGenreIcon(img, genre) {
+    img.setAttribute("class", "movie-img");
+
+    if (mainGenre.includes(genre)) {
+        img.setAttribute("src", `image/section_history/icons/${genre.toLowerCase()}.png`);
+        img.setAttribute("alt", `An icon that represents the movie is ${genre.toLowerCase()} genre`);
+    } else {
+        img.setAttribute("src", `image/section_history/icons/other.png`);
+        img.setAttribute("alt", "An icon with a unpacked box and question mark in the middle");
+    }
+
+    return img
+}
+
+function removeChilds(element) {
+    while (element.firstChild) element.removeChild(element.firstChild); // Removing all childs
 }
 
 // Filtering unique value https://stackoverflow.com/questions/1960473/get-all-unique-values-in-a-javascript-array-remove-duplicates
@@ -318,8 +270,19 @@ function onlyUnique(value, index, array) {
     return array.indexOf(value) === index;
 }
 
+// A function that helps to sort the objects by their watched date https://stackoverflow.com/questions/1129216/sort-array-of-objects-by-string-property-value
+function watchedDateOrder(a, b) {
+    if (a.watchedDate < b.watchedDate) {
+        return 1;
+    }
+    if (a.watchedDate > b.watchedDate) {
+        return -1;
+    }
+    return 0;
+}
+
 // Sort and find users favoruite genre based on Average Rating > Watch Time > Total Duration
-function sortFavGenre(a, b) {
+function favGenreOrder(a, b) {
     var aAvgRating = a.totalRating / a.watchTime;
     var bAvgRating = b.totalRating / b.watchTime;
 
@@ -352,7 +315,7 @@ function sortFavGenre(a, b) {
 
 function favRecommendation(data) {
     const recommendation = document.getElementById('recommendation');
-    while (recommendation.firstChild) recommendation.removeChild(recommendation.firstChild); // Removing all childs
+    removeChilds(recommendation);
 
     let genre = "action";
     for (var i = 0; i < data.length; i++) {
@@ -370,5 +333,98 @@ function favRecommendation(data) {
         recommendation.appendChild(poster);
     }
 
-    console.log(genre)
+    // console.log(genre);
+}
+
+
+
+
+
+
+// Button functions
+function navbarAnimation() {
+    // Nav-bar animation
+    // Create a variable to reference the toggle <button>
+    var navbarToggle = navbar.querySelector("#navbar-toggle");
+
+    // Create a variable to reference the nav menu container <div>
+    var navbarMenu = document.querySelector("#navbar-menu");
+
+    // Create a variable to reference the <ul> list of nav links
+    var navbarLinksContainer = navbarMenu.querySelector(".navbar-links");
+
+    // Add or remove the 'active' class on the toggle <button> when clicked
+    navbarToggle.addEventListener("click", () => { navbarToggle.classList.toggle('active') });
+
+    // Remove the 'active' class on the menu container <div> when clicked 
+    // This will close the menu if the user clicks outside the nav link <ul>
+    navbarMenu.addEventListener("click", () => { navbarToggle.classList.remove('active') });
+
+    // Stop clicks on the navbar links from toggling the menu (for when it's not mobile)
+    navbarLinksContainer.addEventListener("click", (e) => e.stopPropagation());
+}
+
+// A function that will be triggered when the users submit the new movie form
+// Submit function has to be added to the form instead of the button: https://stackoverflow.com/questions/32637920/why-does-a-submit-event-listener-not-work-on-a-submit-button
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    let movieLst = JSON.parse(localStorage.getItem('movieLst')) || [];
+    movieLst.push(getMovieDetails());
+    movieLst.sort(watchedDateOrder);
+    localStorage.setItem('movieLst', JSON.stringify(movieLst));
+    console.log(JSON.parse(localStorage.getItem('movieLst')));
+    form.reset();
+
+    let url = window.location.href;
+    if (url.indexOf("#") != -1) { url = url.split('#')[0] }
+    window.location.href = url + "#history";
+
+    contentUpdate();
+});
+
+showAllBtn.addEventListener("click", () => {
+    const history = document.getElementById('history');
+
+    if (!showAll) {
+        showAllBtn.innerText = "Show Less"
+        history.style.maxHeight = "none";
+        history.style.overflow = "visible";
+        showAll = true;
+    } else {
+        showAllBtn.innerText = "Show All"
+        history.style.maxHeight = "100vh";
+        history.style.overflow = "hidden";
+        showAll = false;
+    }
+
+    sectionOffsetCheck();
+})
+
+function clearHistory() {
+    // Confirmation panel for destructive action https://www.codexworld.com/how-to/show-delete-confirmation-message-dialog-javascript/
+    var confirmation = confirm("Are you sure to clear ALL the history?");
+
+    if (confirmation) {
+        localStorage.removeItem('movieLst');
+        contentUpdate();
+    }
+}
+
+function removeMovie(obj) {
+    let movieLst = JSON.parse(localStorage.getItem('movieLst')) || [];
+    let id = obj.parentNode.parentNode.id;
+
+    for (var i = 0; i < movieLst.length; i++) {
+        if (movieLst[i].uid == id) {
+            movieLst.splice(i, 1);
+            console.log("Movie has been removed");
+            break;
+        }
+    }
+
+    localStorage.setItem('movieLst', JSON.stringify(movieLst));
+    console.log(JSON.parse(localStorage.getItem('movieLst')));
+
+    contentUpdate();
 }
